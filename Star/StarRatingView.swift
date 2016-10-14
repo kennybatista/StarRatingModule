dbc249e866214843d4da37406b548cebdf8305b0
Julia <juliageist23@gmail.com>
2016-10-14 12:33:12 -0700

    refactoring if/else if statement

--- Star/StarRatingView.swift
+++ Star/StarRatingView.swift
@@ -31,39 +31,41 @@ class StarRatingView: UIView {
             starArray.append(star)
             xPosition += width + 10
         }
-        
-        
+    }
+    
+    func touchIsWithinBoundary(location: CGPoint, _ lowerBound: Int, _ upperBound: Int) -> Bool {
+        return (location.x >= CGFloat(lowerBound) && location.x <= CGFloat(upperBound))
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let location = touches.first?.location(in: self)
 //        print(location)
         
-        if ((location?.x)! >= CGFloat(45) && (location?.x)! <= CGFloat(105) ){
+        if (touchIsWithinBoundary(location, 45, 105)){
             let firstStar = starArray[0]
             if firstStar.stateSelected == false {
                 firstStar.shiftToSelectedState()
             }
             
-        } else if ((location?.x)! >= CGFloat(105) && (location?.x)! <= CGFloat(160) ) {
+        } else if (touchIsWithinBoundary(location, 105, 160) ) {
             let secondStar = starArray[1]
             if secondStar.stateSelected == false {
                 secondStar.shiftToSelectedState()
             }
             
-        } else if ((location?.x)! >= CGFloat(160) && (location?.x)! <= CGFloat(223) ) {
+        } else if (touchIsWithinBoundary(location, 160, 223)) {
             let thirdStar = starArray[2]
             if thirdStar.stateSelected == false {
                 thirdStar.shiftToSelectedState()
             }
             
-        } else if ((location?.x)! >= CGFloat(223) && (location?.x)! <= CGFloat(284) ) {
+        } else if (touchIsWithinBoundary(location, 223, 284) ) {
             let fourthStar = starArray[3]
             if fourthStar.stateSelected == false {
                 fourthStar.shiftToSelectedState()
             }
             
-        } else if ((location?.x)! >= CGFloat(284.1) && (location?.x)! <= CGFloat(345) ) {
+        } else if (touchIsWithinBoundary(location, 284, 345) ) {
             let fifthStar = starArray[4]
             if fifthStar.stateSelected == false {
                 fifthStar.shiftToSelectedState()
